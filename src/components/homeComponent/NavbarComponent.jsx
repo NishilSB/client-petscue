ec193f17e04a2c6d08e6bc630af03e7654a43702
Sinsha S B <sinshasb@gmail.com>
2023-10-22 18:16:52 +0530

    user profile

--- src/components/homeComponent/NavbarComponent.jsx
+++ src/components/homeComponent/NavbarComponent.jsx
@@ -1,16 +1,15 @@
 import { useNavigate } from "react-router-dom";
-import {CgProfile} from 'react-icons/cg'
+import { CgProfile } from "react-icons/cg";
+
 const NavbarComponent = () => {
   const navigate = useNavigate();
 
-  const userLoginCheck =JSON.parse( localStorage.getItem('user'))
+  const userLoginCheck = JSON.parse(localStorage.getItem("user"));
 
-  const handleLogout = () =>{
-    localStorage.removeItem('user')
-    navigate('/login')
-  }
-
-
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
 
   return (
     <>
@@ -34,8 +33,7 @@ const NavbarComponent = () => {
               </summary>
               <ul className="absolute left-0 mt-2 py-2 px-3 shadow-md menu dropdown-content bg-base-100 rounded-box w-52">
                 <li>
-                  
-                  <button 
+                  <button
                     onClick={() => navigate("/login")}
                     className="w-full text-left  cursor-pointer"
                   >
@@ -70,43 +68,43 @@ const NavbarComponent = () => {
             </p>
           </div>
         </div>
-        <div className="navbar-end flex justify-end items-center  w-full">
+        <div className="navbar-end flex justify-end items-center  w-full ">
           <div className="">
-              <div className="flex  w-72 gap-6">
-                  {userLoginCheck?
-                  <div className="text-3xl" onClick={()=>navigate('/profile')}> <CgProfile/></div>
-                 
-:<button
-onClick={() => navigate("/register")}
-className="btn bg-red-900 hover:bg-red-950 text-white font-bold  py-4 px-6 border  rounded-3xl "
->
-Sign Up
-</button>
-                  
-}
+            <div className="flex  w-80 gap-6 ">
+              {userLoginCheck ? (
+                <div className="text-5xl mt-1 " onClick={() => navigate("/profile")}>
+                  {" "}
+                  <CgProfile />
+                </div>
+              ) : (
+                <button
+                  onClick={() => navigate("/register")}
+                  className="btn bg-red-900 hover:bg-red-950 text-white font-bold  py-4 px-6 border  rounded-3xl "
+                >
+                  Sign Up
+                </button>
+              )}
 
-                <div className="mr-2  ">
-                  {userLoginCheck? <button
+              <div className="mr-2  ">
+                {userLoginCheck ? (
+                  <button
                     onClick={handleLogout}
                     className="btn bg-red-900 hover:bg-red-950 text-white font-bold py-4 px-8 border border-red-950 rounded-3xl "
-                    
                   >
                     Logout
-                  </button>: <button
+                  </button>
+                ) : (
+                  <button
                     onClick={() => navigate("/login")}
                     className="btn bg-red-900 hover:bg-red-950 text-white font-bold py-4 px-8 border border-red-950 rounded-3xl "
                   >
                     Login
-                  </button>}
-                 
-                 
-                </div>
-
-                <div>
-                </div>
+                  </button>
+                )}
               </div>
-        
-             
+
+              <div></div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/userComponents/ProfileComponent.jsx
+++ src/components/userComponents/ProfileComponent.jsx
@@ -1,83 +1,131 @@
-import { useEffect, useState } from "react";
-import axiosInstance from "../../api/axios"
+import { useEffect, useRef, useState } from "react";
+import axiosInstance from "../../api/axios";
+import axios from "axios";
 
-const ProfileComponent =  () => {
+const ProfileComponent = () => {
+  const [Details, setDetails] = useState("");
+  const fileInputRef = useRef(null);
+  const [image, setImage] = useState("");
 
-  const [Details,setDetails] = useState("")
-
-const userDetails =async()=>{
-
-  const {data} =JSON.parse(localStorage.getItem('user'))
+  const { data } = JSON.parse(localStorage.getItem("user"));
   console.log(data.result.email);
-  const email = data.result.email
+  const email = data.result.email;
+  const userDetails = async () => {
+    const userData = await axiosInstance.post("/user/login", { email });
 
-   const userData= await axiosInstance.post('/user/login',{email})
+    console.log(userData.data.result);
 
-   console.log(userData.data.result);
+    setDetails(userData.data.result);
+  };
 
-   setDetails(userData.data.result)
+  useEffect(() => {
+    userDetails();
+  }, []);
 
-}
+  const handleSvgClick = () => {
+    fileInputRef.current.click();
+  };
 
-useEffect(()=>{
-  userDetails()
-},[])
- 
+  const userImage = async (e) => {
+    if (e.target.files) {
+      const formData = new FormData();
+      const imageUrl = [];
+      const files = e.target.files;
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        formData.append("file", file);
+        formData.append("uplode_preset", "Petscue");
 
+        try {
+          const result = await axios.post(
+            "https://api.cloudinary.com/v1_1/dkglmh9ai/image/upload?upload_preset=petscue",
+            formData
+          );
 
+          console.log(result.data.secure_url);
 
+          imageUrl.push(result.data.secure_url);
+          setImage(result.data.secure_url);
+         
+          console.log(result.data.secure_url);
+
+          if (result.data.secure_url) {
+            changeImage(result.data.secure_url);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+  };
+
+  const changeImage = async (newImage) => {
+    console.log(image);
+    await axiosInstance.post("/user/storeImage", { email, image : newImage });
+  };
+
+  useEffect(() => {
+    axiosInstance.post("/user/login", { email }).then((data) => {
+      console.log(data.data.result.profileImage, "fffffffffffff");
+
+      setImage(data.data.result.profileImage);
+    });
+  }, [email]);
 
   return (
     <>
-      <div style={{ position: "relative" }}>
-        <p
-          className="text-5xl "
-          style={{
-            position: "absolute",
-            top: 200,
-            left: 200, // Change left to right
-            width: "40%",
-            height: "auto",
-          }}
-        >
-          <p className="font-semibold text-lg">Name:   {Details.name}</p>
-          <p className="font-semibold text-lg">Phone Number:   {Details.phone}</p>
-          <p className="font-semibold text-lg">Email:{Details.email}</p>
-          {/* <p className="font-medium" >Pet <a className="text-red-700"> adopt & training</a><br/> in a single page</p> */}
+      <div
+        className="relative flex  w-full h-screen"
+        style={{
+          backgroundImage: `url("/public/images/vector 2.png")`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className=" w-full h-full flex justify-center items-center ">
+          <div>
+            <p className="font-semibold text-lg">Name: {Details.name}</p>
+            <br />
+            <p className="font-semibold text-lg">
+              Phone Number: {Details.phone}
+            </p>
+            <br />
+            <p className="font-semibold text-lg">Email: {Details.email}</p>
+          </div>
+        </div>
+        <div className=" w-full h-full justify-center items-center  flex">
+          <div className="flex-col">
+            {image ? (
+              <div className="  w-80 h-80  rounded-full">
+                <img className="w-80 h-80 rounded-full" src={image} alt="" />
+              </div>
+            ) : (
+              <div className="  w-80 h-80  rounded-full">
+                <img className="w-80 h-80" src="/images/profile.png" alt="" />
+              </div>
+            )}
 
-        </p>
+            <button
+              className="bg-red-900 h-12 mt-4 ml-20 text-white w-36 rounded-lg"
+              onClick={handleSvgClick}
+            >
+              Edit image
+            </button>
 
-        {/* <button className="bg-red-900 hover:bg-red-950 text-white font-bold py-4 px-8 border border-red-950 rounded-3xl " style={{
-            position: "absolute",
-            top: 400,
-            left: 200, // Change left to right
-            height: "auto",
-          }}>
-          Adopt a pet
-        </button> */}
-
-        <img
-          className=""
-          src="/public/images/vector 2.png"
-          alt=""
-          style={{ width: "100%", height: "auto" }}
-        />
-
-        {/* <img
-          className=""
-          src="/public/images/main.png"
-          alt=""
-          style={{
-            position: "absolute",
-            top: 70,
-            right: 40, // Change left to right
-            width: "40%",
-            height: "auto",
-          }}
-        /> */}
+            <div className="ml-3 p-2 rounded-md bg-primary text-white hover:bg-primary-dark transition-colors duration-300">
+              <input
+                type="file"
+                accept="image/*"
+                style={{ display: "none" }}
+                ref={fileInputRef}
+                onChange={userImage}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ProfileComponent
+export default ProfileComponent;
